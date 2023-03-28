8ed7b54cb13cb12fffb9aa8ec15915cae0f7b05f
Fonzie Jacinto <jacintofonzie@gmail.com>
2023-03-28 14:50:38 +0530

    show weather data

--- angular_assignment/src/app/app.component.ts
+++ angular_assignment/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   constructor( private mainService: MainService) {}
 
   search: any;
+  weather: any
 
   public getLocationData() {
     this.mainService.get('http://api.openweathermap.org/geo/1.0/direct?q='+ this.search +'&limit=5&appid=c6694858137c179c803da39fe73dac29').subscribe((data: any) => {
@@ -21,6 +22,7 @@ export class AppComponent {
     public getWeatherData(locationData: any) {
       this.mainService.get('https://api.openweathermap.org/data/2.5/weather?lat='+locationData[0].lat+'&lon='+locationData[0].lon+'&appid=c6694858137c179c803da39fe73dac29').subscribe((data: any) => {
         console.log(data);
+        this.weather = data;
       });
     }
 
