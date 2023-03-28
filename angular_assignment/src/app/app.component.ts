b35228c132977bc5524ee5d53c0193a2a7d8fa6e
Fonzie Jacinto <jacintofonzie@gmail.com>
2023-03-28 14:12:22 +0530

    added api call for search location

--- angular_assignment/src/app/app.component.ts
+++ angular_assignment/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MainService } from './services/main.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+  constructor( private mainService: MainService) {}
+
+  public getLocationData() {
+    this.mainService.get('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=c6694858137c179c803da39fe73dac29').subscribe((data: any) => {
+      console.log(data);
+    });
+  }
+
+
   title = 'angular_assignment';
 }
--- angular_assignment/src/app/app.module.ts
+++ angular_assignment/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './main/main.component';
+import { MainService } from './services/main.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,10 @@ import { MainComponent } from './main/main.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MainService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
