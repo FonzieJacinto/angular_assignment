a3e6d0c145c41f99ef067009b4bc95febaedb527
Fonzie Jacinto <jacintofonzie@gmail.com>
2023-03-28 14:35:19 +0530

    added second weather api call

--- angular_assignment/src/app/app.component.ts
+++ angular_assignment/src/app/app.component.ts
@@ -14,9 +14,15 @@ export class AppComponent {
   public getLocationData() {
     this.mainService.get('http://api.openweathermap.org/geo/1.0/direct?q='+ this.search +'&limit=5&appid=c6694858137c179c803da39fe73dac29').subscribe((data: any) => {
       console.log(data);
+      this.getWeatherData(data);
     });
   }
 
+    public getWeatherData(locationData: any) {
+      this.mainService.get('https://api.openweathermap.org/data/3.0/onecall?lat='+locationData[0].lat+'&lon='+locationData[0].lon+'&appid=c6694858137c179c803da39fe73dac29').subscribe((data: any) => {
+        console.log(data);
+      });
+    }
 
   title = 'angular_assignment';
 }
